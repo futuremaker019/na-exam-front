a8af4881e6565c0f3d2a0f025295aa74c4c4ef64
futuremaker <futuremaker019@gmail.com>
2024-09-10 23:49:23 +0900

    수정

--- src/pages/question/QuestionEditor.tsx
+++ src/pages/question/QuestionEditor.tsx
@@ -2,10 +2,10 @@ import MDEditor from '@uiw/react-md-editor';
 import React, {useState} from 'react';
 
 const QuestionEditor: React.FC = () => {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState<string>('');
     return (
         <div className={"p-10"}>
-            <MDEditor height={300} value={value} onChange={setValue} />
+            <MDEditor height={300} value={value} onChange={(val) => setValue(val || '')} />
         </div>
     );
 };
